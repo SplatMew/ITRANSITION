b1a6d30c96f81f50be707a5cc01d53a1106048c7
Angel Torres <67932472+SplatMew@users.noreply.github.com>
2025-07-30 16:29:17 -0700

    database implemented using postgresql

--- Task 4/server/src/database.js
+++ Task 4/server/src/database.js
@@ -4,18 +4,38 @@ const pool = new Pool({
     user: "postgres",
     password: "AnTo0301!",
     host: "localhost",
-    port: 5432, // Use 5432 unless you configured it to be 8080!
-    database: "postgres", // Connect to the default database
+    port: 5432, 
+    database: "task_4",
 });
 
-pool
-    .query(`CREATE DATABASE TASK_4`)
-    .then((response) => {
-        console.log("Database created");
-        console.log(response); // This will show the query result
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+const createTablQry = `
+    CREATE TABLE accounts(
+        user_id SERIAL PRIMARY KEY,
+        name VARCHAR ( 100 ) NOT NULL,
+        email VARCHAR ( 255 ) NOT NULL,
+        password_hash VARCHAR ( 255 ) NOT NULL,
+        status VARCHAR(10) NOT NULL DEFAULT 'active',
+        created_at TIMESTAMP NOT NULL DEFAULT now(),
+        last_login_at TIMESTAMP
+    );`
+
+const createUniqueEmailIdx = `
+    CREATE UNIQUE INDEX IF NOT EXISTS accounts_email_unique_idx ON accounts(email);
+    `;
+
+const createTimeIndex = `
+    CREATE INDEX IF NOT EXISTS accounts_lastlogin_idx ON accounts (last_login_at)
+    `;
+
+    (async() => {
+        try{
+            await pool.query(createTablQry);
+            await pool.query(createUniqueEmailIdx);
+            await pool.query(createTimeIndex);
+            console.log("Table and unique index ready!");
+        }catch(err){
+            console.error(err);
+        }
+    })();
 
 module.exports = pool;
--- Task 4/server/src/index.js
+++ Task 4/server/src/index.js
@@ -1,16 +1,45 @@
 const express = require('express');
-const cors = require ("cors");
+const cors = require ('cors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const  pool = require("./database.js");
 
-const app = express();
 const PORT = 3000;
 
+const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res)=>{
-    console.log(req.body);
-    //res.send("Response received: ", req.body);
-    res.json(req.body);
-});
+//Middleware to autheticate and check wether the user is active or blocked.
+async function authMiddleWare(req, res, next){
+    const token = req.headers.authorization?.split(" ")[1];
+    if(!token) return res.status(401).json({error: "Missing token."});
+    let payload;
+    try{
+        payload = jwt.verify(token, SECRET);
+    }catch{
+        return res.status(401).json({error: "Invalid or expired token."});
+    }
+
+    const { rows } = await pool.query("SELECT user_id, status FROM accounts WHERE user_is=$1", [payload.id]);
+    if(!rows.length || rows[0].status !== 'active')
+        return res.status(403).json({error:'Account blocked or deleted.'});
+    req.user = rows[0];
+    next();
+}
+
+//Registration endpoint.
+
+//Log in endpoint.
+
+//Block users endpoint
+
+//Get users list, sorted by las login
+
+// Unblock users.
+
+//Delete users.
+
+// testing ping.
 
 app.listen(PORT, ()=> {console.log("server on PORT", PORT)});
