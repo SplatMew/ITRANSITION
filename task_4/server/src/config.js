719944e08b5ce309fad46be53b92fabf15d11285
Angel Torres <67932472+SplatMew@users.noreply.github.com>
2025-08-01 23:00:12 -0700

    fixed typo

--- task_4/server/src/config.js
+++ task_4/server/src/config.js
@@ -6,4 +6,4 @@ export const DB_USER = process.env.DB_USER;
 export const DB_PASSWORD = process.env.DB_PASSWORD;
 export const DB_PORT = process.env.DB_PORT;
 export const HOST_PORT = process.env.HOST_PORT;
-export const DB_URL = process.env.DB_URL;
+export const DB_URL = process.env.DB_URI;
